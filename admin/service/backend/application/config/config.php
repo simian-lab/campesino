94cfbab82572400842597c289d094bca5b0a3e00
Ivan Cano <icano@brandigital.com>
2014-10-08 17:03:54 -0300

    cambios admin

--- admin/service/backend/application/config/config.php
+++ admin/service/backend/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://admin.cyberlunes.com.co/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
