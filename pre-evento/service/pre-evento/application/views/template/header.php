ed8d1cddea26bce9d7a0a03455609c6c306699a8
Ivan Cano <icano@brandigital.com>
2014-10-23 13:49:25 -0300

    pautas

--- pre-evento/service/pre-evento/application/views/template/header.php
+++ pre-evento/service/pre-evento/application/views/template/header.php
@@ -79,16 +79,16 @@
                     <?php endif; ?>               
                     <div class="col-md-6 marcas">
                         <figure class="col-md-3">
-                            <a href="http://wsp.presidencia.gov.co" target="_blank" onclick="onClickPublicidad('Gobierno de Colombia', 'Header')"><img src="<?php echo $base_url_static;?>img/marca1.png" alt="Gobierno de Colombia" /></a>
-                            <img  width=0 height=0 src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" onload="_paq.push(['trackEvent', 'Pauta', 'impressions', 'Gobierno de Colombia']);">
-                        </figure>
-                        <figure class="col-md-3">
-                            <a href="http://www.mintic.gov.co" target="_blank" onclick="onClickPublicidad('MinTIC', 'Header')"><img src="<?php echo $base_url_static;?>img/marca2.png" alt="MinTIC" /></a>
+                            <a href="http://www.mintic.gov.co" target="_blank" onclick="onClickPublicidad('MinTIC', 'Header')"><img src="<?php echo $base_url_static;?>img/mintic_mobile.png" alt="MinTIC" /></a>
                             <img  width=0 height=0 src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" onload="_paq.push(['trackEvent', 'Pauta', 'impressions', 'MinTIC']);">
                         </figure>
                         <figure class="col-md-3">
-                            <a href="http://www.vivedigital.gov.co" target="_blank" onclick="onClickPublicidad('Vive digital', 'Header')"><img src="<?php echo $base_url_static;?>img/marca3.png" alt="Vive digital" /></a>
+                            <a href="http://www.vivedigital.gov.co" target="_blank" onclick="onClickPublicidad('Vive digital', 'Header')"><img src="<?php echo $base_url_static;?>img/digital_mobile.png" alt="Vive digital" /></a>
                             <img  width=0 height=0 src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" onload="_paq.push(['trackEvent', 'Pauta', 'impressions', 'Vive digital']);">
+                        </figure>
+                        <figure class="col-md-3">
+                            <a href="http://wsp.presidencia.gov.co" target="_blank" onclick="onClickPublicidad('Gobierno de Colombia', 'Header')"><img src="<?php echo $base_url_static;?>img/colombia_mobile.png" alt="Gobierno de Colombia" /></a>
+                            <img  width=0 height=0 src="data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" onload="_paq.push(['trackEvent', 'Pauta', 'impressions', 'Gobierno de Colombia']);">
                         </figure>
                     </div>
                 </div>                             
