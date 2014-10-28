bffcd5cba8c77064147d379a750d11d409d03e8e
Ivan Cano <icano@brandigital.com>
2014-10-28 17:37:51 -0300

    mencached

--- evento/service/evento/application/config/testing/memcached.php
+++ evento/service/evento/application/config/testing/memcached.php
@@ -9,7 +9,7 @@ $memcached['servers'] = array(
 
 	'default' => array(
 
-			'host'			=> 'memcached-cyberlunes.ma1isw.cfg.use1.cache.amazonaws.com:11211',
+			'host'			=> 'memcached-cyberlunes.ma1isw.cfg.use1.cache.amazonaws.com',
 			'port'			=> '11211',
 			'weight'		=> '1',
 			'persistent'	=> FALSE
