8d9e862a6f3221d5c9d6706a5af99ed64e6de645
Ivan Cano <icano@brandigital.com>
2014-11-05 17:41:52 -0300

    piwik

--- evento/service/evento/application/views/piwik.php
+++ evento/service/evento/application/views/piwik.php
@@ -60,6 +60,27 @@
 
          <?php
       break;
+      case 'origin':
+          ?>
+              <!-- Piwik -->
+              <script type="text/javascript">
+                var _paq = _paq || [];
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                  var u="//ceet.piwikpro.com/";
+                  _paq.push(['setTrackerUrl', u+'piwik.php']);
+                  _paq.push(['setSiteId', 48]);
+                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+                })();
+              </script>
+              <noscript><p><img src="//ceet.piwikpro.com/piwik.php?idsite=48" style="border:0;" alt="" /></p></noscript>
+              <!-- End Piwik Code -->
+
+
+         <?php
+      break;
       case 'production':
       ?>
           <!-- Piwik -->
