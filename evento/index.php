667dec31b98ffb2063a7e0d17ff6b971651cf650
Ivan Cano <icano@brandigital.com>
2014-11-05 14:08:47 -0300

    origin

--- evento/index.php
+++ evento/index.php
@@ -50,6 +50,16 @@ switch ($_SERVER['SERVER_NAME'])
 			);
         define('ENVIRONMENT', 'testing');
 	break;
+	case 'origin-www2.cyberlunes.com.co':	 
+		$dominios_permitidos = array(
+				'http://origin-www2.cyberlunes.com.co',
+				'https://origin-www2.cyberlunes.com.co',
+				'http://static-stage.cyberlunes.com.co',
+				'https://static-stage.cyberlunes.com.co',
+				'http://fonts.gstatic.com/'
+			);
+        define('ENVIRONMENT', 'origin');
+	break;
 	case 'www.cyberlunes.com.co':	 
 		$dominios_permitidos = array(
 				'http://www.cyberlunes.com.co',
@@ -84,6 +94,7 @@ if (defined('ENVIRONMENT'))
 			ini_set('display_errors', TRUE);
 			ini_set('display_startup_errors', TRUE);
 		break;
+		case 'origin':
 		case 'testing':
 			error_reporting(0);
 		break;
