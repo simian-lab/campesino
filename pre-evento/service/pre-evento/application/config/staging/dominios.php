e7b001d4a89f1e1e00ff97cf295dc8fe8fe79ba6
Ivan Cano <icano@brandigital.com>
2014-11-12 11:39:28 -0300

    staging

---
+++ pre-evento/service/pre-evento/application/config/staging/dominios.php
@@ -0,0 +1,23 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Website details
+|
+| These details are used in emails sent by authentication library.
+|----
+ */
+/*
+admin.eltiempo-co-cyberlunes.live.brandigital.com
+static.eltiempo-co-cyberlunes.live.brandigital.com
+evento.eltiempo-co-cyberlunes.live.brandigital.com
+pre-evento.eltiempo-co-cyberlunes.live.brandigital.com
+static.eltiempo-co-cyberlunes.stage.brandigital.com
+evento.eltiempo-co-cyberlunes.stage.brandigital.com
+pre-evento.eltiempo-co-cyberlunes.stage.brandigital.com
+*/
+
+$config['base_site_eltiempo'] = 'http://staging.cyberlunes.com.co/';
+$config['base_site_eltiempo_static'] = 'http://d3b369zdeuh99v.cloudfront.net/';
+$config['base_site_eltiempo_root'] = 'http://staging.cyberlunes.com.co/';
+
---
+++ pre-evento/service/pre-evento/application/config/staging/email.php
@@ -0,0 +1,16 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Website details
+|
+| These details are used in emails sent by authentication library.
+|----
+ */
+
+$config['website_email'] = 'ggiorda@brandigital.com';
+$config['website_sender'] = 'promocionescyberlunes@eltiempo.com';
+$config['website_contacto'] = 'ggiorda@brandigital.com';
+$config['website_name'] = '';
+$config['smtp_host'] = 'localhost';
+$config['email_to'] = array('pathen@eltiempo.com','nelfer@eltiempo.com','jean1052@gmail.com','yohmor@eltiempo.com');
