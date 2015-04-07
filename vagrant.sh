dda5657a7357f7fb430ca7d771b75689fecc04d0
Ivan Vasquez <ivancamilov@gmail.com>
2015-04-07 14:17:15 -0500

    agregada base de datos

--- vagrant.sh
+++ vagrant.sh
@@ -2,7 +2,15 @@
 rm -rf /var/www/public
 ln -fs /vagrant /var/www/public
 
-cp /vagrant/stack/admin-virtualhost.conf /etc/apache2/sites-available/admin.confa2ensite admin
+# Make sure log folder exists
+mkdir /vagrant/logs
+
+# Create a database
+mysql --user=root --password=root < /vagrant/stack/eltiempo_co_cyber_lunes_live2.sql
+
+# Create the Websites
+cp /vagrant/stack/admin-virtualhost.conf /etc/apache2/sites-available/admin.conf
+a2ensite admin
 
 # Restart for good measure
 service apache2 restart -y
