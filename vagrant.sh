8da7c38e751cd5535c51ac52c4afe4136d37a7bc
Ivan Vasquez <ivancamilov@gmail.com>
2015-04-07 14:16:37 -0500

    finalizada configuración de vhost de admin

--- vagrant.sh
+++ vagrant.sh
@@ -2,4 +2,7 @@
 rm -rf /var/www/public
 ln -fs /vagrant /var/www/public
 
-cp /vagrant/stack/admin-virtualhost.conf /etc/apache2/sites-available/admin.conf
+cp /vagrant/stack/admin-virtualhost.conf /etc/apache2/sites-available/admin.confa2ensite admin
+
+# Restart for good measure
+service apache2 restart -y
