e220e695c4363ff1293e14c5da3ea92abdec4bf4
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2015-04-10 08:34:01 -0500

    Creada la función que se encarga de obtener el ID del usuario.

--- admin/service/backend/application/modules/aliado/models/aliado_model.php
+++ admin/service/backend/application/modules/aliado/models/aliado_model.php
@@ -1,25 +1,25 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+class Aliado_model extends CI_Model {
 
-class Aliado_model extends CI_Model
-{
+	public function __construct() {
+		parent::__construct();
+	}
 
-  	public function __construct()
-  	{
-  		parent::__construct();
-  	}
+  function control_aliado($pUSER_ID){
+    $param = array(
+       $pUSER_ID  // ,pUSER_ID INT
+    );
 
+    $sql = "CALL AB_PATROCINADORES_INSERT(?)";
+    $query = $this->db->query($sql, $param);
+    return $query->result_array();
+  }
 
-
-    function control_aliado($pUSER_ID){
-      $param = array(
-         $pUSER_ID  // ,pUSER_ID INT
-      );
-
-      $sql = "CALL AB_PATROCINADORES_INSERT(?)";
-      $query = $this->db->query($sql, $param);
-      return $query->result_array();
-    }
-
-
+  function get_store_id($user_id) {
+    $param = array($user_id);
+    $sql = "CALL GET_USER_STORE_ID(?)";
+    $query = $this->db->query($sql, $param);
+    return $query->result_array();
+  }
 }
