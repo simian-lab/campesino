22f40d47b7e9b592e6fb0f0121d133eea20f17fe
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2015-04-10 10:36:56 -0500

    Añadida la columna de aliados a la tabla de patrocinadores.

--- admin/service/backend/application/modules/aliados/controllers/aliados.php
+++ admin/service/backend/application/modules/aliados/controllers/aliados.php
@@ -40,7 +40,7 @@ class Aliados extends Main {
 
 			$crud->fields('PAT_ID','PAT_NOMBRE','PAT_LOGO','PAT_URL', 'PAT_URL_EVENT', 'PAT_FECHA','VISIBILITY','PAT_USER_CREADOR','PAT_USER_ULTIMO', 'PAT_PAQUETE', 'PAT_ALIADO');
 			$crud->required_fields('PAT_NOMBRE','PAT_LOGO','PAT_FECHA','VISIBILITY');
-			$crud->columns('PAT_NOMBRE','PAT_FECHA','VISIBILITY', 'PAT_PAQUETE');
+			$crud->columns('PAT_NOMBRE','PAT_FECHA','VISIBILITY', 'PAT_PAQUETE', 'PAT_ALIADO');
 
 			$crud->callback_before_update(array($this,'encrypt_password_callback'));
 
