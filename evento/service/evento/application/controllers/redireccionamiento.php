4454bc59a14ed4ece8beee7921d2ad081b833304
Ivan Vasquez <ivancamilov@gmail.com>
2015-04-21 10:09:33 -0500

    formato? :D

--- evento/service/evento/application/controllers/redireccionamiento.php
+++ evento/service/evento/application/controllers/redireccionamiento.php
@@ -61,7 +61,7 @@ class Redireccionamiento extends CI_Controller {
       Both Patrocinadores and Promociones will have a PRO_URL at this point
       (that is, if they have a valid URL). The fields are named differently
       in the DB, but we set an alias in promocion_model so that it's easier
-      to handle in here (we're saving ourselves a bunch of if statements).
+      to handle in here (we're saving ourselves a bunch of if statements)
       */
       $result_base['PRO_URL']= $this->_sanitiseURL($result_base['PRO_URL']);
 
