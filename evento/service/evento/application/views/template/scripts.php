f5f1c9b27c8c15373ce0f800d34d53fcdc5d97e2
Ivan Vasquez <ivancamilov@gmail.com>
2015-04-21 15:38:40 -0500

    ajustes de diseño de Dinamik

--- evento/service/evento/application/views/template/scripts.php
+++ evento/service/evento/application/views/template/scripts.php
@@ -1,6 +1,5 @@
 <script src="<?php echo $base_url_static;?>js/bootstrap/bootstrap.js"></script>
 <script src="<?php echo $base_url_static;?>js/vendor/owl.carousel.js"></script>
-<script src="<?php echo $base_url_static;?>js/main.js"></script>
 
 <!-- js temporal de dynamik -->
 <script src="http://dynamikdemos.a2hosted.com/clients/eltiempo/cyberlunes/temp.js"></script>
