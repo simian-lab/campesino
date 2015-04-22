e6b3393ff563054300e6993024a3c4de750d38ea
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2015-04-22 15:54:57 -0500

    CYBER-119 - Añadida la columna de Ciudad en la tabla de registros.

--- admin/service/backend/application/modules/formulario_participacion/controllers/formulario_participacion.php
+++ admin/service/backend/application/modules/formulario_participacion/controllers/formulario_participacion.php
@@ -29,6 +29,7 @@ class formulario_participacion extends Main {
         $crud->display_as('US_EMPRESA','Empresa')
 	         ->display_as('US_NOMBRE','Nombre Contacto')
 	         ->display_as('US_CARGO','Cargo')
+	         ->display_as('US_CIUDAD','Ciudad')
 	         ->display_as('US_EMAIL','Email')
 	         ->display_as('US_TELEFONO_MOVIL','Celular')
 	         ->display_as('US_URL_TIENDA','Tienda')
@@ -51,7 +52,7 @@ class formulario_participacion extends Main {
         $crud->unset_add();
         $crud->unset_edit();
 		$crud->fields('US_EMPRESA','US_NOMBRE','US_CARGO','US_EMAIL','US_TELEFONO_MOVIL','US_URL_TIENDA','US_TELEFONO_OFICINA','US_COMENTARIOS');
-        $crud->columns('US_EMPRESA','US_NOMBRE','US_CARGO','US_EMAIL','US_TELEFONO_MOVIL','US_URL_TIENDA','US_TELEFONO_OFICINA','US_COMENTARIOS');
+        $crud->columns('US_EMPRESA', 'US_CIUDAD', 'US_NOMBRE','US_CARGO', 'US_EMAIL','US_TELEFONO_MOVIL','US_URL_TIENDA','US_TELEFONO_OFICINA','US_COMENTARIOS');
 		$this->data= array('autor'=>$this->session->userdata('username') . ' ('.$this->session->userdata('email').')' , 'ident'=>$this->session->userdata('sadmin_user_id') );
 
 	 
