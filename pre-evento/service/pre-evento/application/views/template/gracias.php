2a531f4326a73678edd9ff5f00e2c65e6c4f210c
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2015-04-24 16:36:06 -0500

    Añadidos nuevos pixeles de conversión de Facebook.

--- pre-evento/service/pre-evento/application/views/template/gracias.php
+++ pre-evento/service/pre-evento/application/views/template/gracias.php
@@ -41,6 +41,20 @@
 		</script>
 		<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/offsite_event.php?id=6015078203399&amp;value=0.01&amp;currency=EUR" /></noscript>
 
+		<!-- Facebook Conversion Code for Registros Cyber Lunes -->
+		<script>(function() {
+			var _fbq = window._fbq || (window._fbq = []);
+			if (!_fbq.loaded) { var fbds = document.createElement('script'); fbds.async = true; fbds.src = '//connect.facebook.net/en_US/fbds.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(fbds, s); _fbq.loaded = true; }
+		})();
+		window._fbq = window._fbq || [];
+		window._fbq.push(['track', '6028444610680',
+			{'value':'0.00','currency':'COP'}
+			]);
+		</script>
+		<noscript>
+			<img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6028444610680&cd[value]=0.00&cd[currency]=COP&noscript=1" />
+		</noscript>
+
 		<!-- Google Tag Manager -->
 		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-NGBVTZ"
 		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
