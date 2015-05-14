69ab47024cf7e5cc99421b3fa7d9492f7c501ed2
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2015-05-14 10:34:33 -0500

    Escapadas las entidades HTML en el evento.

--- evento/service/evento/application/modules/articulos/views/detalleArticulo.php
+++ evento/service/evento/application/modules/articulos/views/detalleArticulo.php
@@ -4,6 +4,7 @@
         <date><?php echo convertirFecha($articulo[0]['ART_FECHA']) ?></date>
     </hgroup>                   
     <figure>
+    	<?php $articulo[0]['ART_IMAGEN'] = htmlentities($articulo[0]['ART_IMAGEN'], ENT_QUOTES); ?>
         <img src="<?php echo $base_url_img_articulos ?><?php echo $articulo[0]['ART_IMAGEN'] ?>" alt="">
     </figure>     
     <p><?php echo htmlspecialchars_decode($articulo[0]['ART_DETALLE']) ?></p>
