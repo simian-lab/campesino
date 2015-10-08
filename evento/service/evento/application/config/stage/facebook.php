415da1887c322e13e0103f296fceb61eab10a3ae
giosyst3m <me@giosyst3m.net>
2015-10-08 10:43:09 -0500

    Ambiente Stage

---
+++ evento/service/evento/application/config/stage/facebook.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Datos para configuracion de FACEBOOK 
+ * 
+ */
+
+$config = array(
+    'FB_APPID' => '1628539567435754',
+    'FB_SECRET' => 'd3e61a5a02f8c4fb0755ee139a54a931',
+    'FB_SCOPE' => '',
+    'FB_BASEURL' => '',
+    'FB_APPBASEURL' => ''
+);
+
