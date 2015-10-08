8db42485a9617d44e70c8713c8d3d2f0c0ac7d93
grigev <grigev@eltiempo.com>
2015-10-08 16:05:34 -0500

    * config de stage para admin

---
+++ admin/service/backend/application/config/stage/email.php
@@ -0,0 +1,16 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Website details
+|
+| These details are used in emails sent by authentication library.
+|----
+
+ */
+
+
+$config['smtp'] = 'localhost';
+$config['mail_send'] = 'promocionescyberlunes@eltiempo.com';
+
+
