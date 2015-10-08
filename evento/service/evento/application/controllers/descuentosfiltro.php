7257a635b207491128c4acf37ea3a40c095f8f87
giosyst3m <me@giosyst3m.net>
2015-10-08 14:25:32 -0500

    Ajuste MX_Controller

--- evento/service/evento/application/controllers/descuentosfiltro.php
+++ evento/service/evento/application/controllers/descuentosfiltro.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Descuentosfiltro extends Default_Controller
+class Descuentosfiltro extends MX_Controller
 {
     function __construct()
     {
