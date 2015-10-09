182f4514a58865a43cd54d286a6746ca72209f40
giosyst3m <me@giosyst3m.net>
2015-10-09 14:08:46 -0500

    Ajustes Static para Admin y Evento Stage

--- evento/service/evento/application/config/stage/xdomain.php
+++ evento/service/evento/application/config/stage/xdomain.php
@@ -3,6 +3,6 @@
 *
 */
 $config['allowed_domains'] = array(
-    'http://static-sss-'.EVENTO.'-stage.loencontraste.com',
+    'http://static-sss-stage.loencontraste.com',
     ' http://sss-'.EVENTO.'-stg.loencontraste.com'
 ); 
