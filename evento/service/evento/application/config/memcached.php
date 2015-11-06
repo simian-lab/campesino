c9a183be10cbf3ddcd35742bc7173ed8ef75ca1c
Ivan Vasquez <ivancamilov@gmail.com>
2015-11-06 15:07:05 -0500

    ajustes de backend de memcached

--- evento/service/evento/application/config/memcached.php
+++ evento/service/evento/application/config/memcached.php
@@ -24,10 +24,10 @@ $memcached['config'] = array(
 
 	'engine'                => 'Memcache',				// Set which caching engine you are using. Acceptable values: Memcached or Memcache
 	'prefix' 				=> ENVIRONMENT,						// Prefixes every key value (useful for multi environment setups)
-	'compression'			=> TRUE,					// Default: FALSE or MEMCACHE_COMPRESSED Compression Method (Memcache only).
+	'compression'			=> 0,					// Default: FALSE or MEMCACHE_COMPRESSED Compression Method (Memcache only).
 
 	// Not necessary if you already are using 'compression'
-	'auto_compress_tresh'	=> ENVIRONMENT,					// Controls the minimum value length before attempting to compress automatically.
+	//'auto_compress_tresh'	=> ENVIRONMENT,					// Controls the minimum value length before attempting to compress automatically.
 	'auto_compress_savings'	=> 0.2,						// Specifies the minimum amount of savings to actually store the value compressed. The supplied value must be between 0 and 1.
 
 	'expiration'			=> 3600,					// Default content expiration value (in seconds)
