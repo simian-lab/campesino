5d59a043d99180792239a3317c61f9b744ee60dc
zheiran <juansebastiang2@gmail.com>
2015-11-06 11:44:07 -0500

    Diccionario omniture funcionando

--- omniture_generator/generator.php
+++ omniture_generator/generator.php
@@ -1,6 +1,6 @@
 <?php
 // TODO: Unify this database configuration with the xml_parser.php
-/*if(!$argv) {
+if(!$argv) {
   error("Este script solo se debe ejecutar por consola.");
 }
 if(count($argv) != 2) {
@@ -33,79 +33,72 @@ else {
     default:
     error("Entorno '".$argv[1]."' inválido. Las opciones válidas son 'development', 'stage', 'production' y 'origin'");
     break;
-  }*/
-  $DB_HOST = 'localhost';
-    $DB_NAME = 'eltiempo_co_cyber_lunes_v2';
-    $DB_USERNAME = 'root';
-    $DB_PASSWORD = 'root';
-    /*______________*/
+  }
   try {
     $DB = new PDO( "mysql:host=".$DB_HOST.";"."dbname=".$DB_NAME, $DB_USERNAME, $DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   }
   catch(PDOException $e) {
     error($e->getMessage());
   }
-  //if($argv[1] != 'production') {
-    $FTP_CREDENTIALS['SERVER'] = 'ftp3. omniture.com';
-    $FTP_CREDENTIALS['USER'] = 'eltiempoeltiempoalafijade _616765';
-    $FTP_CREDENTIALS['PASSWORD'] = 'Ybi3 yrYb';
-  /*}
+  if($argv[1] != 'production') {
+    $FTP_CREDENTIALS['SERVER'] = 'ftp3.omniture.com';
+    $FTP_CREDENTIALS['USER'] = 'eltiempoeltiempoalafijade_616765';
+    $FTP_CREDENTIALS['PASSWORD'] = 'Ybi3yrYb';
+  }
   else {
-    $FTP_CREDENTIALS['SERVER'] = 'ftp3. omniture.com';
-    $FTP_CREDENTIALS['USER'] = 'eltiempoel tiempoalafijap_2533821';
-    $FTP_CREDENTIALS['PASSWORD'] = 'yDBj* $s5';
+    $FTP_CREDENTIALS['SERVER'] = 'ftp3.omniture.com';
+    $FTP_CREDENTIALS['USER'] = 'eltiempoeltiempoalafijap_2533821';
+    $FTP_CREDENTIALS['PASSWORD'] = 'yDBj*$s5';
   }
   init($DB, $FTP_CREDENTIALS);
-}*/
+}
 function init($db, $ftp_credentials) {
     $file_name = "omniture.tab";
     $date = date_format(date_create(), 'Y-m-d H:i:s');
     $header = "## SC    SiteCatalyst SAINT Import File  v:2.1\n"
     . "## SC    '## SC' indicates a SiteCatalyst pre-process header. Please do not remove these lines.\n"
     . "## SC    D:" . $date . " A:2864550:51\n";
-    $table_header = "\nKey  ID-Anterior Nombre  Categoria   Sub-Categoria   Tipo    Contenido   Comentarios Precios Configuracion\n";
-    $query_specials = $db->prepare("SELECT * FROM specials");
-    $query_specials->execute();
-    $specials = $query_specials->fetchAll();
-    if(!empty($specials)) {
+    $table_header = "\nKey ID-Anterior Nombre Categoria Sub-Categoria Tipo Contenido Comentarios Precios Configuracion\n";
+    $query_eventos = $db->prepare("SELECT * FROM EVE_EVENTOS");
+    $query_eventos->execute();
+    $eventos = $query_eventos->fetchAll();
+    if(!empty($eventos)) {
         $file = fopen($file_name, "w");
         fwrite($file, $header);
         fwrite($file, $table_header);
-        foreach($specials as $special) {
-            if($special['slug'] != NULL) {
-                add_promotions_from_special($db, $file, $special);
-            }
+        foreach($eventos as $evento) {
+          add_promotions_from_special($db, $file, $evento);
         }
         fclose($file);
         upload_file_ftp($ftp_credentials, $file_name);
     }
 }
-function add_promotions_from_special($db, $file, $special) {
-    $special_id = $special['id'];
-    $special_prefix = strtoupper(substr($special['slug'], 0, 3));
-    $key_prefix = 'ESP' . $special_prefix . '-';
-    $query_promotions = $db->prepare("SELECT * FROM PRO_PROMOCIONES WHERE PRO_ESPECIAL_ID = :special_id");
-    $query_promotions->bindParam(':special_id', $special_id);
+function add_promotions_from_special($db, $file, $evento) {
+    $evento_id = $evento['EVE_ID'];
+    $evento_prefix = $evento['EVE_PREFIJO'];
+    $key_prefix = 'ofe' . $evento_prefix .'-';
+    $query_promotions = $db->prepare("SELECT * FROM PRO_PROMOCIONES INNER JOIN EXP_EVENTOXPROMOCION ON  PRO_PROMOCIONES.PRO_ID=EXP_EVENTOXPROMOCION.EXP_PROMOCION  WHERE EXP_EVENTO = :evento_id");
+    $query_promotions->bindParam(':evento_id', $evento_id);
     $query_promotions->execute();
     $promotions = $query_promotions->fetchAll();
     foreach($promotions as $promotion) {
-        $query_taxonomy = $db->prepare("SELECT * FROM TAX_TAXONOMIAS WHERE TAX_ID = :tax_id");
-        $query_taxonomy->bindParam(':tax_id', $promotion['PRO_TAX_ID']);
-        $query_taxonomy->execute();
-        $taxonomy = $query_taxonomy->fetchAll();
+        $query_category = $db->prepare("SELECT * FROM CAT_CATEGORIA WHERE CAT_ID = :cat_id");
+        $query_category->bindParam(':cat_id', $promotion['CAT_ID']);
+        $query_category->execute();
+        $category = $query_category->fetchAll();
+        $category_slug = str_replace('-', '_', $category[0]['CAT_SLUG']);
 
-        $subcategory_slug = str_replace('-', '_', $taxonomy[0]['TAX_SLUG']);
-        $query_taxonomy = $db->prepare("SELECT TAX_SLUG FROM TAX_TAXONOMIAS WHERE TAX_ID = :tax_id");
-        $query_taxonomy->bindParam(':tax_id', $taxonomy[0]['TAX_PARENT_ID']);
-        $query_taxonomy->execute();
-        $taxonomy = $query_taxonomy->fetchAll();
+        $query_subcategory = $db->prepare("SELECT * FROM SUB_SUBCATEGORIA WHERE SUB_ID = :subcat_id");
+        $query_subcategory->bindParam(':subcat_id', $promotion['SUB_ID']);
+        $query_subcategory->execute();
+        $subcategory = $query_subcategory->fetchAll();
+        if(empty($subcategory)) {
+            $subcategory_slug = 'NA';
+        } else {
+          $subcategory_slug = str_replace('-', '_', $subcategory[0]['SUB_SLUG']);
+        }
 
-        if(!empty($taxonomy)) {
-            $category_slug = str_replace('-', '_', $taxonomy[0]['TAX_SLUG']);
-        }
-        else {
-            $category_slug = 'NA';
-        }
+
 
         $row = $key_prefix . $promotion['PRO_ID']
         . ' 0'
