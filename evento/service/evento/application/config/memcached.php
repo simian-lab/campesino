d3b7b198e782e8f2c025675d46ac7fdfdd966181
Ivan Camilo Vásquez <ivancamilov@gmail.com>
2015-11-06 15:27:52 -0500

    Update memcached.php

--- evento/service/evento/application/config/memcached.php
+++ evento/service/evento/application/config/memcached.php
@@ -27,7 +27,7 @@ $memcached['config'] = array(
 	'compression'			=> 0,					// Default: FALSE or MEMCACHE_COMPRESSED Compression Method (Memcache only).
 
 	// Not necessary if you already are using 'compression'
-	//'auto_compress_tresh'	=> ENVIRONMENT,					// Controls the minimum value length before attempting to compress automatically.
+	'auto_compress_tresh'	=> ENVIRONMENT,					// Controls the minimum value length before attempting to compress automatically.
 	'auto_compress_savings'	=> 0.2,						// Specifies the minimum amount of savings to actually store the value compressed. The supplied value must be between 0 and 1.
 
 	'expiration'			=> 3600,					// Default content expiration value (in seconds)
