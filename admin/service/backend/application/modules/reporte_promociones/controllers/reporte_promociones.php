1f5617a7908e2e7fa693df5db58f2840276cff79
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2015-11-11 14:07:13 -0500

    LN-240: Cantidad de promociones aprobadas en los reportes de promociones.

--- admin/service/backend/application/modules/reporte_promociones/controllers/reporte_promociones.php
+++ admin/service/backend/application/modules/reporte_promociones/controllers/reporte_promociones.php
@@ -21,10 +21,10 @@ class Reporte_promociones extends Main {
 		$crud = new grocery_CRUD();
 
 		if($group_id == 4 || $group_id == 3) {
-			$crud->columns('PAT_ID', 'PAT_NOMBRE', 'PAQ_NOMBRE', 'EVE_NOMBRE', 'PAQ_MONTO_PREMIUM', 'PAQ_CREADAS_PREMIUM', 'PAQ_MONTO_PREMIUM_CATEGORIA', 'PAQ_CREADAS_PREMIUM_CATEGORIA', 'PAQ_MONTO_GENERAL', 'PAQ_CREADAS_GENERAL');
+			$crud->columns('PAT_ID', 'PAT_NOMBRE', 'PAQ_NOMBRE', 'EVE_NOMBRE', 'PAQ_MONTO_PREMIUM', 'PAQ_CREADAS_PREMIUM', 'PAQ_APROBADAS_PREMIUM', 'PAQ_MONTO_PREMIUM_CATEGORIA', 'PAQ_CREADAS_PREMIUM_CATEGORIA', 'PAQ_APROBADAS_PREMIUM_CATEGORIA', 'PAQ_MONTO_GENERAL', 'PAQ_CREADAS_GENERAL', 'PAQ_APROBADAS_GENERAL');
 		}
 		else {
-			$crud->columns('PAQ_NOMBRE', 'EVE_NOMBRE', 'PAQ_MONTO_PREMIUM', 'PAQ_CREADAS_PREMIUM', 'PAQ_MONTO_PREMIUM_CATEGORIA', 'PAQ_CREADAS_PREMIUM_CATEGORIA', 'PAQ_MONTO_GENERAL', 'PAQ_CREADAS_GENERAL');
+			$crud->columns('PAQ_NOMBRE', 'EVE_NOMBRE', 'PAQ_MONTO_PREMIUM', 'PAQ_CREADAS_PREMIUM', 'PAQ_APROBADAS_PREMIUM', 'PAQ_MONTO_PREMIUM_CATEGORIA', 'PAQ_CREADAS_PREMIUM_CATEGORIA', 'PAQ_APROBADAS_PREMIUM_CATEGORIA', 'PAQ_MONTO_GENERAL', 'PAQ_CREADAS_GENERAL', 'PAQ_APROBADAS_GENERAL');
 			$crud->or_where('PAT_ID', $user_id);
 		}
 
@@ -34,10 +34,13 @@ class Reporte_promociones extends Main {
 		$crud->display_as('EVE_NOMBRE', 'Evento');
 		$crud->display_as('PAQ_MONTO_PREMIUM', 'Cantidad Premium Asignadas');
 		$crud->display_as('PAQ_CREADAS_PREMIUM', 'Cantidad Premium Creadas');
+		$crud->display_as('PAQ_APROBADAS_PREMIUM', 'Cantidas Premium Aprobadas');
 		$crud->display_as('PAQ_MONTO_PREMIUM_CATEGORIA', 'Cantidad Premium Categoría Asignadas');
 		$crud->display_as('PAQ_CREADAS_PREMIUM_CATEGORIA', 'Cantidad Premium Categoría Creadas');
+		$crud->display_as('PAQ_APROBADAS_PREMIUM_CATEGORIA', 'Cantidad Premium Categoría Aprobadas');
 		$crud->display_as('PAQ_MONTO_GENERAL', 'Cantidad General Asignadas');
 		$crud->display_as('PAQ_CREADAS_GENERAL', 'Cantidad General Creadas');
+		$crud->display_as('PAQ_APROBADAS_GENERAL', 'Cantidad General Aprobadas');
 
 		$crud->set_primary_key('AXP_ID');
 		$crud->set_table('V_REPORTE_PROMOCIONES');
