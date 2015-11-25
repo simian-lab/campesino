219019e459ef4d967fcd9e460c0c990c5a1e4bcb
zheiran <juansebastiang2@gmail.com>
2015-11-25 11:55:43 -0500

    Ajuste a ofenav

--- omniture_generator/generator.php
+++ omniture_generator/generator.php
@@ -70,7 +70,7 @@ function init($db, $ftp_credentials) {
         upload_file_ftp($ftp_credentials, $file_name);
 }
 function add_promotions_from_special($db, $file) {
-    $key_prefix = 'evenav-';
+    $key_prefix = 'ofenav-';
     $query_promotions = $db->prepare("SELECT * FROM PRO_PROMOCIONES");
     $query_promotions->execute();
     $promotions = $query_promotions->fetchAll();
