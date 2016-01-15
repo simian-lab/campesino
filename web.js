66e369af7c05e14235f6b7de31b96342e692d16e
zheiran <juansebastiang2@gmail.com>
2016-01-15 13:56:09 -0500

    First commit

---
+++ web.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use('/', function(req, res, next) {
+  var host = req.header('host');
+  next();
+});
+
+app.use('/', express.static(__dirname + '/www'));
+app.listen(process.env.PORT || 5000);
