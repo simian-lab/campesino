9520c58c52d72fdec51353c836d90ca675627044
Nicolás Saiz Arismendi <nicolasaiz173@gmail.com>
2016-07-28 15:26:49 -0500

    We minify now a lot of things.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,38 @@
 'use strict'
 
-var gulp = require('gulp'),
+var cssNano = require('gulp-cssnano'),
+	htmlMin = require('gulp-htmlmin'),
+	imageMin = require('gulp-imagemin'),
+	gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	bourbon = require('node-bourbon'),
 	browserSync = require("browser-sync").create(),
-	reload = browserSync.reload;
+	rev = require('gulp-rev'),
+	uglify = require('gulp-uglify'),
+	usemin = require('gulp-usemin');
+
+// Minify HTML
+gulp.task('html', function() {
+	gulp.src('prod/index.html')
+	.pipe(htmlMin({collapseWhitespace: true}))
+	.pipe(gulp.dest('prod'));
+});
+
+// Minify our images.
+gulp.task('images', function() {
+	gulp.src('dev/images/**/*')
+	.pipe(imageMin())
+	.pipe(gulp.dest('prod/images'));
+});
 
 //Task for reloading the page
 gulp.task('reload', function() {
   browserSync.reload();
-  
+});
+
+gulp.task('root-files', function() {
+	gulp.src('dev/*.{ico,png,txt,xml}')
+	.pipe(gulp.dest('prod'));
 });
 
 //Static server + watching scss/html files
@@ -31,6 +54,14 @@ gulp.task('sass', function () {
 	.pipe(reload({stream:true}))
 });
 
+gulp.task('usemin', function() {
+	gulp.src('dev/index.html')
+	.pipe(usemin({
+		css: [cssNano(), rev()]
+	}))
+	.pipe(gulp.dest('prod'));
+});
+
 //Task for watching scss/html/js files changes
 gulp.task('watch', function(){
 	gulp.watch('dev/**/*.scss', ['sass']);
@@ -41,3 +72,8 @@ gulp.task('watch', function(){
 //Task by default
 gulp.task('default', ['sass', 'serve', 'watch']);
 
+/* 
+IMPORTANT! For production run 'gulp prod' and then 'gulp html'.
+Or just 'gulp prod; gulp html;'
+*/
+gulp.task('prod', ['usemin', 'images', 'root-files']);
